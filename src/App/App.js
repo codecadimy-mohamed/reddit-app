345dffc25a37a6fa3c0a428d1e6b2c1401788fb5
mohamed ouanjine <email.for.tv.elhajji@gmail.com>
2023-07-06 18:09:34 +0200

    add Main component

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,18 @@
 import SearchPage from "../Components/searchPage/SearchPage";
 import { createBrowserRouter, createRoutesFromChildren, Route, RouterProvider } from 'react-router-dom';
 import TopPosts from "../features/topPosts/TopPosts";
+import Main from "../Components/Main/Main";
 
 const appRouter = createBrowserRouter(createRoutesFromChildren(
-  <Route path='/search' element={ <SearchPage /> }>
-    <Route index element={ <TopPosts /> } />
+  <Route path="/" element={<Main />}>
+    <Route path='/search' element={<SearchPage />}>
+      <Route index element={<TopPosts />} />
+    </Route>
   </Route>
 ));
 function App() {
   return (
-    <RouterProvider router={appRouter}/>
+    <RouterProvider router={appRouter} />
   );
 }
 
